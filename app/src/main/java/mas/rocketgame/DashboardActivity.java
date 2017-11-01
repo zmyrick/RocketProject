4f74403dc5d712aa449d85c596e15b3eead7bdd3
Zack Myrick <zmyrick3@gatech.edu>
2017-11-01 00:16:56 -0400

    Added measurement button

--- app/src/main/java/mas/rocketgame/DashboardActivity.java
+++ app/src/main/java/mas/rocketgame/DashboardActivity.java
@@ -1,13 +1,30 @@
 package mas.rocketgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.database.Cursor;
+import android.content.CursorLoader;
+import android.app.LoaderManager.LoaderCallbacks;
 
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(android.R.style.Theme_Holo);
         setContentView(R.layout.activity_dashboard);
+
+
+        Button takeMeasurementButton = (Button) findViewById(R.id.measurement_button);
+        takeMeasurementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
